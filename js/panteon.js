b7fc43357aaf5e3722fca8491637f9e40e128cd6
TigranPoghos <tigran2002poghosyan@gmail.com>
2024-04-01 22:41:47 +0300

    finish

--- js/panteon.js
+++ js/panteon.js
@@ -41,12 +41,58 @@ document.addEventListener("DOMContentLoaded", function(){
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
         },
-
         breakpoints: {
             768: {
                 slidesPerView: 2,
             }, 
+            320: {
+                slidesPerView: 1,
+            }
         },
     });
+
+
+
+
+    const burger__js = document.querySelector('.burger__js')
+    const burger__jsBody = document.querySelector('.burger__jsBody')
+    const header = document.querySelector('.header')
+    burger__js.addEventListener('click', function(){
+        burger__jsBody.classList.toggle('active')
+        header.classList.toggle('active')
+    })
+
+    document.addEventListener('click', (e) => {
+        const clickBody = e.composedPath().includes(burger__jsBody)
+        const click = e.composedPath().includes(burger__js)
+        const clickHeader = e.composedPath().includes(header)
+    
+        if (( !clickBody ) && (!click) && (!clickHeader))  {
+            burger__jsBody.classList.remove('active')
+            header.classList.toggle('active')
+        }
+    })
+
+
+
+
+    const burger__footer = document.querySelector('.burger__footer')
+    const burger__footerBody = document.querySelector('.burger__footerBody')
+    const footer__info = document.querySelector('.footer__info')
+    burger__footer.addEventListener('click', function(){
+        burger__footerBody.classList.toggle('active')
+        footer__info.classList.toggle('active')
+    })
+
+    document.addEventListener('click', (e) => {
+        const clickfooterBody = e.composedPath().includes(burger__footerBody)
+        const clickfooter = e.composedPath().includes(burger__footer)
+        const clickInfo = e.composedPath().includes(footer__info)
+    
+        if (( !clickfooterBody ) && (!clickfooter) && (!clickInfo))  {
+            burger__footerBody.classList.remove('active')
+            footer__info.classList.toggle('active')
+        }
+    })
       
 })
